dd97446d5623fb60ce0c1f22e0fe8527eb6759b4
Oleg Fomenko <oleg.fomenko2002@gmail.com>
2019-09-26 16:38:31 +0300

    css was added

--- AlfaFront/js/validation.js
+++ AlfaFront/js/validation.js
@@ -1,6 +1,6 @@
 function check(login, password) {
 	var isValid = true;
-	if(login.indexOf('@') == -1) isValid = false;
+	if(login.indexOf('@') == -1 || login.indexOf('.') == -1) isValid = false;
 	if(password.length < 8) isValid = false;
 
 	return isValid;
