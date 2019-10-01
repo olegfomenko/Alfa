5c69293d03cef768858b1af47430ddab215dc648
Oleg Fomenko <oleg.fomenko2002@gmail.com>
2019-10-01 20:27:41 +0300

    bugfix, @ReponseBody annotation was added

--- AlfaBack/src/main/java/com/oleg/fomenko/AlfaBack/rest/addUser.java
+++ AlfaBack/src/main/java/com/oleg/fomenko/AlfaBack/rest/addUser.java
@@ -3,10 +3,7 @@ package com.oleg.fomenko.AlfaBack.rest;
 import com.oleg.fomenko.AlfaBack.database.UserDatabaseController;
 import com.oleg.fomenko.AlfaBack.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class addUser {
@@ -15,8 +12,7 @@ public class addUser {
 
     @CrossOrigin
     @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public User add(String userName, String userPassword) {
-        User user = new User(userName, userPassword);
+    public User add(@RequestBody User user) {
         userDatabaseController.add(user);
         return user;
     }
